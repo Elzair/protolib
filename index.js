42962ee6ebf4fffa3f1e9a31f615823d000d1d22
Philip Woods <elzairthesorcerer@gmail.com>
2014-09-08 10:42:44 -0400

    Added base methods: mixin, clone, and inherit

---
+++ index.js
@@ -0,0 +1,29 @@
+exports.mixin = function(object, base) {
+  for (var property in base) {
+    object[property] = base[property];
+  }
+};
+
+exports.clone = function(object) {
+  var object_clone = {};
+  for (var property in object) {
+    object_clone[property] = object[property]; 
+  }
+  return object_clone;
+};
+
+exports.inherit = function(proto) {
+  if (Object.create) {            // Use ES5 Object.create
+    return Object.create(proto);
+  }
+  else if ({}.__proto__) {        // Use non-standard property
+    var object = {};
+    object.__proto__ = proto;
+    return object;
+  }
+  else {                          // Fallback to using constructor
+    var Constructor = function(){};
+    Constructor.prototype = proto;
+    return new Constructor();
+  }
+};
