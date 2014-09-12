1375a099fba476946857549458e488ffe489dcce
Elzair <elzairthesorcerer@gmail.com>
2014-09-12 16:55:05 -0400

    Factored majority of clone method into its own helper function

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
-var typeOf = require('typeof');
+var clone_helper = require(__dirname + '/lib/clone-helper')
+  , typeOf       = require('typeof');
 
 exports.clone = function(object) {
-  if (typeOf(object) !== 'object' && typeOf(object) !== 'array') {
-    throw new Error("Cannot clone!");
-  }
-
-  var object_clone = typeOf(object) === 'array' ? [] : {};
-
-  for (var property in object) {
-    if (typeOf(object[property]) === 'function') {
-      object_clone[property] = object[property].bind(object_clone);
-    }
-    else if (typeOf(object[property]) === 'date') {
-      object_clone[property] = new Date(object[property].toString());
-    }
-    else if (typeOf(object[property]) === 'object'){
-      object_clone[property] = exports.clone(object[property]); 
-    }
-    else {
-      object_clone[property] = object[property];
-    }
-  }
-
-  return object_clone;
+  return clone_helper(object);
 };
 
 exports.inherit = function(proto) {
