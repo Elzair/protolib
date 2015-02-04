a505154c224f0e1d3518e54ba54730cff165b5ba
Philip Woods <elzairthesorcerer@gmail.com>
2015-02-04 14:18:22 -0500

    Removed backwards compatibility kludges from function inherit in index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 "use strict";
 
 var clone_helper = require(__dirname + '/lib/clone-helper')
-  , poly         = require('polyfill-function-prototype-bind')
   , typeOf       = require('elucidata-type');
 
 exports.clone = function(object) {
@@ -17,19 +16,7 @@ exports.clone = function(object) {
 };
 
 exports.inherit = function(proto) {
-  if (Object.create) {            // Use ES5 Object.create
-    return Object.create(proto);
-  }
-  else if ({}.__proto__) {        // Use non-standard property
-    var object = {};
-    object.__proto__ = proto;
-    return object;
-  }
-  else {                          // Fallback to using constructor
-    var Constructor = function(){};
-    Constructor.prototype = proto;
-    return new Constructor();
-  }
+  return Object.create(proto);
 };
 
 exports.mixin = function(object, proto) {
